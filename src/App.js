1d86425d11623ff2243bd29af0a3fa3d82f06d6a
MitanshuShaBa <42906426+MitanshuShaBa@users.noreply.github.com>
2020-12-20 00:03:15 +0530

    Household and Community Pages merged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./Layout/Navbar";
-import home from "./Layout/home";
+import Community from "./Community";
 import { useStateValue } from "./Layout/StateProvider";
 import {
   BrowserRouter as Router,
@@ -8,6 +8,8 @@ import {
   Route,
   useHistory,
 } from "react-router-dom";
+import { Component } from "react";
+import Household from "./Household";
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
@@ -15,43 +17,20 @@ function App() {
   return (
     <Router>
       <div>
-         <Navbar />
-        <div style={{ minHeight: window.innerHeight }}>
+        <Navbar />
+        <div>
           <Switch>
-            <Route path="/"
-            component={home} 
-            />
-            <Route path="/register">
-              <h1>Account</h1>
+            <Route path="/community">
+              <Community />
             </Route>
-            <Route path="/log">
-              <h1>Hello</h1>
+            <Route path="/household">
+              <Household />
             </Route>
-            <Route path="/login">
-              <h1>Account</h1>
-            </Route>
-            <Route path="/cart">
-              <h1>Account</h1>
-              <Route path="/"
-            component={home} />
+            <Route path="/">
+              <h1>Home Page</h1>
             </Route>
           </Switch>
         </div>
-        <footer
-          style={{
-            position: "static",
-            right: 0,
-            bottom: 0,
-            width: "100%",
-            color: "white",
-            textAlign: "right",
-            marginTop: "15vh",
-          }}
-        >
-          <a href="https://www.freepik.com/vectors/logo">
-            Logo vector created by catalyststuff - www.freepik.com
-          </a>
-        </footer>
       </div>
     </Router>
   );
--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,47 +1,174 @@
 import React from "react";
-import {
-  makeStyles
-} from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import clsx from "clsx";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import HomeIcon from "@material-ui/icons/Home";
+import PeopleIcon from "@material-ui/icons/People";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    
+    display: "flex",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: "tomato",
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
-    
   },
-  title: {
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3),
   },
-  AppBar: {
-    backgroundColor: "#e53935",
-    position: "fixed",
-  }
-
+  cards: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  card: {
+    width: "200px",
+    marginTop: "5vh",
+  },
 }));
 
 export default function Navbar() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const navItems = [
+    { text: "Household", icon: <HomeIcon /> },
+    { text: "Community", icon: <PeopleIcon /> },
+  ];
 
-  return ( 
-    <div className = {classes.root}>
-      <AppBar position = "static" className={classes.AppBar}>
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="sticky"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
         <Toolbar>
-          <IconButton edge = "start" className = {classes.menuButton} color = "inherit" aria-label = "menu">
-            <MenuIcon/>
-          </IconButton> 
-          <Typography variant = "h6" className = {classes.title}>Appartment 1101</Typography> <Button color = "inherit" >Login</Button>
-        </Toolbar> 
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Society Name
+          </Typography>
+        </Toolbar>
       </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {navItems.map(({ text, icon }, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
     </div>
-    
   );
 }
